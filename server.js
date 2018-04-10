8f4089dc72eefb4c1e4cbe129ee9a588f8019695
Cecilia Clohisy <cclohisy@gmail.com>
2018-04-10 14:56:16 -0500

    server up path working

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 // Your server.js file should require the basic npm packages we've used in class: 
-//express, body-parser and path.
-
+//express, body-parser and path. 
+//dependencies....
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+  });
+  
 
 //var user1Scores =[]
 //var user2Scores =[]
